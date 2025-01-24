fd60bb416129e00bcbd414ec2b517f23526cf88f
Daniil Savik <savikdaniil22@gmail.com>
2025-01-24 12:55:26 +0300

    feat(burger-menu): add burger menu functionality, interface, and responsiveness

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
 const burgerButton = document.querySelector(".header__burger");
+const menuContent = document.querySelector(".header__menu-content");
+const overlay = document.querySelector(".header__overlay");
+const closeButton = document.querySelector(".header__menu-close");
 
-burgerButton.addEventListener("click", function () {
-  alert("Кнопка работает!");
+function closeMenu() {
+  menuContent.classList.remove("header__menu-content--active");
+  overlay.classList.remove("header__overlay--active");
+}
+
+burgerButton.addEventListener("click", () => {
+  menuContent.classList.toggle("header__menu-content--active");
+  overlay.classList.toggle("header__overlay--active");
+});
+
+closeButton.addEventListener("click", () => {
+  closeMenu();
+});
+
+overlay.addEventListener("click", () => {
+  closeMenu();
 });
